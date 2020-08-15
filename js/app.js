53efa9733189aa8ebaefd03113f4150c4f18fe3e
Ali <62906333+ali1987isaev@users.noreply.github.com>
2020-08-15 07:28:31 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -112,34 +112,113 @@ function deleteTaskFromDOM(e) {
 }
 
 function error() {
-  const popup = document.querySelector("[data-errorPopup]");
-  popup.style.visibility = 'visible';
+  if(!document.querySelector('[data-add-task]').classList.contains('disabled')) {
+    const closePopup= ()=> popup.style.visibility = 'hidden';
+    const popup = document.querySelector("[data-errorPopup]");
+    popup.style.visibility = 'visible';
 
-  document.querySelector("[data-popup-btn]")
-    .addEventListener('click', closePopup)
-  document.querySelector("[data-popup-symbol]")
-    .addEventListener('click', closePopup)
-
-  function closePopup() {
-    popup.style.visibility = 'hidden'
+    document.querySelector("[data-popup-btn]").addEventListener('click', closePopup)
+    document.querySelector("[data-popup-symbol]").addEventListener('click', closePopup)
   }
 }
-/*
 
-git --version
-git config --global user.name "Ali Isaev"
-git config --global user.email "ali1987isaev@gmail.com"
+// Registration is activated. Adding classes:
+document.querySelector('[data-add-task]').classList.add('disabled');
+document.querySelector('.addTask_text').classList.add('disabled');
 
-clear
+// Register form events and functions:
+(function (){
+  const registrationPopup = document.querySelector('.registrationForm-bg');
+    // Todo-List activation function and events:
+    function removeDisabled() {
+      document.querySelector('.addTask_text').classList.remove('disabled');
+      document.querySelector('[data-add-task]').classList.remove('disabled');
+      document.querySelector('[data-add-task]').classList.add('active');
+      registrationPopup.style.visibility = 'hidden';
+    }
+    // Registration form activation. Events:
+    document.querySelector('[data-add-task]').addEventListener('click', ()=> {
+      document.querySelector('[data-add-task]').classList.contains('disabled')
+       ? registrationPopup.style.visibility = 'visible' : null;
+    })
+  
+  ///////////////////////////////////////////////
+  // check length input:
+  function checkLength(input, min, max) {
+    if (input.value.length < min) {
+      showError(input, `${firstLetter(input)} must not be less than ${min} characters`);
+    } else if (input.value.length > max) {
+      showError(input, `${firstLetter(input)} must be no more than ${max} characters`);
+    } else {
+      showSuccess(input);
+    }
+  }
 
-git add "test.txt"
-gitk
-git status
-git commit -m "New comment about this project"
+  // error & success functions:
+  function showError(input, message) {
+    const parentEl = input.parentElement;
+    parentEl.classList.remove("success");
+    parentEl.classList.add("error");
+    const small = parentEl.querySelector("small");
+    small.innerText = message;
+  }
+  function showSuccess(input) {
+    const parentEl = input.parentElement;
+    parentEl.classList.remove("error");
+    parentEl.classList.add("success");
+  }
 
-git add *
-git commit -m "... new comment..."
+  // first letter to uppercase function:
+  function firstLetter(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  }
 
-git log  (project history) ctrl+z (exit commit)
+  // passwords identity check function:
+  function comparePasswods(input1, input2) {
+    if (input1.value !== input2.value) {
+      showError(input2, `${firstLetter(input2)} is not valid`);
+    }
+  }
 
-*/
+  // email validation function:
+  function validateEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(input.value).toLowerCase())) {
+      showSuccess(input);
+    } else {
+      showError(input, `${firstLetter(input)} is not valid`);
+    }
+  }
+
+  // UI elements:
+  const formSubmit = document.querySelector('[data-registration-form]'),
+    username = document.querySelector("[data-userName]"),
+    email = document.querySelector("[data-userEmail]"),
+    password = document.querySelector("[data-userPassword]"),
+    passwordConfirm = document.querySelector("[data-userPasswordConfirm]"),
+    formFields = document.querySelectorAll('.formFields');
+
+  // Checking registration form and activation Todo-List:
+  function todoListActivated(e){  
+      formFields[0].classList.contains('success') 
+      && formFields[1].classList.contains('success') 
+      && formFields[2].classList.contains('success') 
+      && formFields[3].classList.contains('success')
+      && document.querySelector('[data-add-task]').classList.contains('disabled')
+        ? removeDisabled()
+        : console.error('Form validation error');
+  }
+
+  // Form events:
+  formSubmit.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkLength(passwordConfirm, 6, 25);
+    comparePasswods(password, passwordConfirm);
+    validateEmail(email);
+    todoListActivated(e);
+  });
+})()
+
